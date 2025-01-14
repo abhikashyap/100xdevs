e25eb04c220ef3a726a7e807f1df9745cb59dd3f
abhikashyap <abhikashyap.ds@gmail.com>
2025-01-15 05:03:50 +0530

    commit1

---
+++ week9 react/vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return <div>
+    <b>
+      hi
+    </b>
+    <Counter></Counter>
+    </div>
+}
+function Counter(){
+  return <div>
+    <h1>
+      <button>
+        Increase count
+      </button>
+    </h1>
+  </div>
+}
+
+
+export default App
